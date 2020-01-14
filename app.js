8917e491deac5b0d91a8013f75bcf5cd8e6d5cf1
polly <miru11104627@gmail.com>
2020-01-13 21:07:23 -0500

    add player2 alternate player

--- app.js
+++ app.js
@@ -9,14 +9,46 @@ GAME RULES:
 
 */
 
-var scores, roundScores,activePlayer,dice;
+var scores, roundScores,activePlayer;
 scores=[0,0];
 roundScores=0;
 activePlayer=0;
-dice=Math.floor(Math.random()*6)+1;
-console.log(dice);
-/*player1 current scores*/
-$("#current-"+activePlayer).html(dice);
-/*hide dice at the beginner of the game*/
 
+
+/*player1 current scores*/
+// $("#current-"+activePlayer).html(dice);
+/*hide dice at the beginner of the game*/
 $(".dice").css("display","none");
+$(".btn-roll").click(function(){
+    //create an random number
+     var dice=Math.floor(Math.random()*6)+1;
+    //dispaly result 
+    $(".dice").css("display","block");
+    $(".dice").attr("src",'dice-'+dice+'.png');
+    //update round score if the rolled number is not 1
+    
+
+    if (dice!=1){
+        //add score
+        roundScores+=dice;
+        $("#current-"+activePlayer).html(roundScores);
+        $(".player-1-panel").removeClass("active");
+        $(".player-0-panel").addClass("active");
+
+        
+    
+    }else{
+        //pass to next player(ternary)
+        activePlayer===0? activePlayer =1 : activePlayer =0;
+        console.log(activePlayer);
+        
+        var lastActivePlayer=Math.abs(activePlayer-1);
+        $("#current-"+lastActivePlayer).html(0);
+        roundScores=0;
+        console.log("test1");
+        $(".player-0-panel").removeClass("active");
+        $(".player-1-panel").addClass("active");
+
+
+    }
+});
