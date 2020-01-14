ab9badc3c82e189121aa3a8a1f0e74d8a15edbe6
polly <miru11104627@gmail.com>
2020-01-14 00:39:06 -0500

    replace app.js add inhance start and hold function;

--- app.js
+++ app.js
@@ -9,77 +9,75 @@ GAME RULES:
 
 */
 
-var scores, roundScores,activePlayer;
+var scores, roundScores, activePlayer, gamePlaying;
 init();
-/*player1 current scores*/
-// $("#current-"+activePlayer).html(dice);
-/*hide dice at the beginner of the game*/
-$(".dice").css("display","none");
-$(".btn-roll").click(function(){
-    //create an random number
-     var dice=Math.floor(Math.random()*6)+1;
-    //dispaly result 
-    $(".dice").css("display","block");
-    $(".dice").attr("src",'dice-'+dice+'.png');
-    //update round score if the rolled number is not 1
-    
+/*game switch*/
+$('.dice').css('display', 'none');
 
-    if (dice!=1){
-        //add score
-        roundScores+=dice;
-        $("#current-"+activePlayer).html(roundScores);
- 
-    }else{
-
-        nextPlayer();
-
-    }
+$('.btn-roll').click(function() {
+	if (gamePlaying) {
+		//create an random number
+		var dice = Math.floor(Math.random() * 6) + 1;
+		//dispaly result
+		$('.dice').css('display', 'block');
+		$('.dice').attr('src', 'dice-' + dice + '.png');
+		//update round score if the rolled number is not 1
+		if (dice != 1) {
+			//add score
+			roundScores += dice;
+			$('#current-' + activePlayer).html(roundScores);
+		} else {
+			nextPlayer();
+		}
+	}
 });
-
-//hold function 
-$(".btn-hold").click(function(){
-    //add current to global
-    scores[activePlayer]+=roundScores;
-    roundScores=0;
-    //update UI
-    $("#score-"+activePlayer).html(scores[activePlayer]); 
-    //if player win 
-    if (scores[activePlayer]>=10){
-       
-        $("#name-"+activePlayer).html("winner!!");
-        $(".dice").css("display","none");
-        $(".player-"+activePlayer+"-panel").addClass("winner");
-        $(".player-"+activePlayer+"-panel").removeClass("active");
-    }else{
-    nextPlayer();
-    }
+//hold function
+$('.btn-hold').click(function() {
+	if (gamePlaying) {
+		//add current to global
+		scores[activePlayer] += roundScores;
+		roundScores = 0;
+		//update UI
+		$('#score-' + activePlayer).html(scores[activePlayer]);
+		//if player win
+		if (scores[activePlayer] >= 10) {
+			$('#name-' + activePlayer).html('winner!!');
+			$('.dice').css('display', 'none');
+			$('.player-' + activePlayer + '-panel').addClass('winner');
+			$('.player-' + activePlayer + '-panel').removeClass('active');
+			gamePlaying = false;
+		} else {
+			nextPlayer();
+		}
+	}
 });
 
 //statrt a new game
-$('.btn-new').click(function(){
-    init();
-    $(".player-0-panel").addClass("active");
-    $(".player-1-panel").removeClass("active");
-    $(".player-"+activePlayer+"-panel").removeClass("winner");
-    $("#name-0").html("player-1");
-    $("#name-1").html("player-2");
-
+$('.btn-new').click(function() {
+	init();
+	$('.player-0-panel').addClass('active');
+	$('.player-1-panel').removeClass('active');
+	$('.player-' + activePlayer + '-panel').removeClass('winner');
+	$('#name-0').html('player-1');
+    $('#name-1').html('player-2');
+   
 });
 
-function nextPlayer(){
-    activePlayer===0? activePlayer=1:activePlayer=0;
-    roundScores=0;
-    $("#current-"+activePlayer).html(0);
-    $(".player-0-panel").toggleClass("active");
-    $(".player-1-panel").toggleClass("active");
-    $(".dice").css("display","none");
-
+function nextPlayer() {
+    $('#current-' + activePlayer).html(0);
+	activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+	roundScores = 0;
+	$('#current-' + activePlayer).html(0);
+	$('.player-0-panel').toggleClass('active');
+	$('.player-1-panel').toggleClass('active');
+	$('.dice').css('display', 'none');
 }
 
-function init(){
-    scores=[0,0];
-    roundScores=0;
-    activePlayer=0;
-
-
+function init() {
+	scores = [ 0, 0 ];
+	roundScores = 0;
+	activePlayer = 0;
+    gamePlaying = true;
+    $(".player-score").html(0);
+    $(".player-current-score").html(0);
 }
