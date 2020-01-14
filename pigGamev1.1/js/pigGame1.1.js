eb9fecc22f937b80af0d1a35af694af387999080
polly <miru11104627@gmail.com>
2020-01-14 17:22:59 -0500

    enable to set win point

--- pigGamev1.1/js/pigGame1.1.js
+++ pigGamev1.1/js/pigGame1.1.js
@@ -1,7 +1,8 @@
 /*
 GAME RULES:
 
-1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable) continuous 2, 6
+1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. 
+
 2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
 3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
 */
@@ -44,8 +45,22 @@ $('.btn-hold').click(function() {
 		roundScores = 0;
 		//update UI
 		$('#score-' + activePlayer).html(scores[activePlayer]);
+		//get final win number
+		if(input){
+			var winPoint=$(".finalScore").val();
+			console.log(winPoint);
+			$(".btn-win").click(function() {
+				$(".point-win").html(winPoint);
+				console.log(winPoint);
+			});
+
+		}else{
+			winPoint=100;
+		}
+	
+	
 		//if player win
-		if (scores[activePlayer] >= 10) {
+		if (scores[activePlayer] >=winPoint) {
 			$('#name-' + activePlayer).html('winner!!');
 			$('.dice').css('display', 'none');
 			$('.player-' + activePlayer + '-panel').addClass('winner');
